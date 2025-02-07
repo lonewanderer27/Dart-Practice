40f947b7500d0c48bcb83123b8b8b270bc07263b
lonewanderer27 <adrianejames27@gmail.com>
2025-02-07 22:47:50 +0800

    feat(practice3): ask the user what num to compare to

--- practice3/bin/practice3.dart
+++ practice3/bin/practice3.dart
@@ -3,13 +3,18 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   // print the numbers that are less than 5, or specific no.
-
+  int? num;
   var nums = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
 
-  print('These are the nums that are less than 5');
+  stdout.write("Give me a number to compare it to: ");
+  num = int.tryParse(stdin.readLineSync()!);
+
+  num ??= 5;
+
+  print('These are the nums that are less than $num');
   for (var i = 0; i < nums.length; i++) {
-    if (nums[i] < 5) {
-      print(nums[i]);
+    if (nums[i] < num) {
+      stdout.write('${nums[i]}${ i+1 < nums.length ? ", " : "" }');
     }
   }
 }
